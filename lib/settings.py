ef68c5140006532ee501a41a17cc06b8532a7db6
InfamousSYN <ainfamoussyn@gmail.com>
2021-03-12 04:24:25 +0000

    updated version setting

--- lib/settings.py
+++ lib/settings.py
@@ -2,7 +2,7 @@
 import os
 
 # Application Version
-__version__ = "0.0.0.1"
+__version__ = "0.0.0.2"
 
 # Directory Mapping
 root_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'wipen'))
